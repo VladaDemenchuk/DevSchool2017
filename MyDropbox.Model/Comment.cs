c2326616ee1699b20c64eeea684204652da1b97a
DemenchukVlada <vladavlada94@rambler.ru>
2017-05-10 15:13:04 +0200

    add model

---
+++ MyDropbox.Model/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyDropbox.Model
+{
+    public class Comment
+    {
+        public Guid UserId { get; set; }
+        public Guid FileId { get; set; }
+        public string Text { get; set; }
+    }
+}
