a93ac76a84284cb5f49332c07c060ff254e88d75
DemenchukVlada <vladavlada94@rambler.ru>
2017-05-23 00:11:43 +0200

    add ISharesRepository and SharesRepository

---
+++ MyDropbox.DataAccess/ISharesRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyDropbox.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDropbox.DataAccess
+{
+    public interface ISharesRepository
+    {
+        void Add(Share share);
+        void Delete(Share share);
+        IEnumerable<File> GetUserFiles(Guid userId);
+    }
+}
---
+++ MyDropbox/SharesRepository.cs
@@ -0,0 +1,76 @@
+﻿using MyDropbox.DataAccess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDropbox.Model;
+using System;
+using System.Data.SqlClient;
+
+namespace MyDropbox
+{
+    public class SharesRepository : ISharesRepository
+    {
+        private readonly string _connectionString;
+        private readonly IUsersRepository _usersRepository;
+        private readonly IFilesRepository _filesRepository;
+
+        public SharesRepository(string connectionString, IFilesRepository filesRepository)
+        {
+            _connectionString = connectionString;
+            _filesRepository = filesRepository;
+        }
+
+        public void Add(Share share)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "insert into Share (FileId, UserId) values (@FileId, @UserId)";
+                    command.Parameters.AddWithValue("@FileId", share.FileId);
+                    command.Parameters.AddWithValue("@UserId", share.UserId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(Share share)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "delete from Share where FileId = @FileId and UserId = @UserId";
+                    command.Parameters.AddWithValue("@FileId", share.FileId);
+                    command.Parameters.AddWithValue("@UserId", share.UserId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public IEnumerable<File> GetUserFiles(Guid userId)
+        {
+            var result = new List<File>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "select FileId from Share where UserId = @UserId";
+                    command.Parameters.AddWithValue("@UserId", userId);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(_filesRepository.GetInfo(reader.GetGuid(reader.GetOrdinal("FileId"))));
+                        }
+                        return result;
+                    }
+                }
+            }
+        }
+    }
+}
